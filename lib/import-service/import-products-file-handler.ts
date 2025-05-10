5fd9f3d0adf15b5bdb2dc01e9448077773eb3d01
duncannevin <duncan.nevin@gmail.com>
2025-05-10 11:05:29 -0700

    add tests for import products

--- lib/import-service/import-products-file-handler.ts
+++ lib/import-service/import-products-file-handler.ts
@@ -1,18 +1,18 @@
 import {S3Client, PutObjectCommand} from '@aws-sdk/client-s3';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 
-const s3 = new S3Client([
-  {region: process.env.AWS_REGION!},
-]);
-
-const BUCKET_NAME = process.env.BUCKET_NAME!;
-
 export interface ImportProductsFileEvent {
   fileName: string;
 }
 
 export async function main(event: ImportProductsFileEvent): Promise<{result: string, signedUrl: string}> {
   console.log('Import Products File Handler:', event);
+
+  const BUCKET_NAME = process.env.BUCKET_NAME!;
+
+  const s3 = new S3Client([
+    {region: process.env.AWS_REGION!},
+  ]);
 
   const fileName = event.fileName;
 
